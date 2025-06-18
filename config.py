cb7c7715e8137f6f92788bbaa681721694d3b50b
Prateeksha Mehta <pratu2930@gmail.com>
2025-06-18 15:18:14 -0700

    Fix binding to 0.0.0.0 and update requirements

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'APforever@2930',
-    'database': 'GradAccreditation',
+    host:os.environ.get("DB_HOST"),
+    user: os.environ.get("DB_USER"),
+    password:os.environ.get("DB_PASSWORD"),
+    database:os.environ.get("DB_NAME"),
 }
