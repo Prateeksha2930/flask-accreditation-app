705beca467a6054de554a9d8771307dc2cbfaf60
Prateeksha Mehta <pratu2930@gmail.com>
2025-06-18 15:30:10 -0700

    Fix config.py keys with quotes for DB config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# config.py
+import os
 
 DB_CONFIG = {
-    host:os.environ.get("DB_HOST"),
-    user: os.environ.get("DB_USER"),
-    password:os.environ.get("DB_PASSWORD"),
-    database:os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME"),
 }
