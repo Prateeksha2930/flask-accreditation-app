65c092059e576d8a22462eafa20647cd038049a9
Prateeksha Mehta <pratu2930@gmail.com>
2025-06-18 21:45:36 -0700

    Fix config.py keys with quotes for DB config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ DB_CONFIG = {
     "host": os.environ.get("MYSQL_ADDON_HOST"),
     "user": os.environ.get("MYSQL_ADDON_USER"),
     "password": os.environ.get("MYSQL_ADDON_PASSWORD"),
-    "port" : os.environ.get("MYSQL_ADDON_PORT"),
+    "port" : int(os.environ.get("MYSQL_ADDON_PORT")),
     "database": os.environ.get("MYSQL_ADDON_DB")
 }
